7f41e926799f0e566cbdd80af515d87d8ca4cfd7
felixcatto <felix0808@yandex.ru>
2017-03-30 18:47:43 +0400

    meteor: mapp, change ability to edit lists name

--- meteor/mapp/imports/ui/components/lists.js
+++ meteor/mapp/imports/ui/components/lists.js
@@ -1,19 +1,28 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
+import { ReactiveVar } from 'meteor/reactive-var';
 import './lists.html';
 
 
 
 Template.lists.onCreated(function() {
+    this.editingId = new ReactiveVar(null);
     this.getListId = function(el) {
-        const item = $(el).parent('.js-list');
+        const item = $(el).parents('.js-list');
         return item.length == 0 ? null : item.attr('data-id');
     };
 });
 
 
+Template.lists.helpers({
+    editing(list) {
+        return Template.instance().editingId.get() == list._id;
+    },
+});
+
+
 Template.lists.events({
-    'keydown input[type=text]'(event) {
+    'keydown .js-add-list'(event) {
         if (event.which == 13) {
             const input = event.target;
             Meteor.call('lists.insert', input.value);
@@ -26,7 +35,18 @@ Template.lists.events({
     },
     'click .js-edit-list'(event, instance) {
         const id = instance.getListId(event.target);
-        // console.log('sorry, this is doesn\'t work now :\'(')
-        // Meteor.call('lists.remove', id);
+        Template.instance().editingId.set(id);
+        Tracker.flush();
+        instance.$('.js-edit-input').focus();
+    },
+    'keydown .js-edit-input'(event, instance) {
+        if (event.which == 13) {
+            const input = event.target;
+            Meteor.call('lists.updateName', instance.getListId(input), input.value);
+            Template.instance().editingId.set(null);
+        }
+        else if (event.which == 27) {
+            Template.instance().editingId.set(null);
+        }
     },
 });
