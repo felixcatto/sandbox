c4405d743d5da087d5b2f8ddcc8aa4d48c1ced22
felixcatto <felix0808@yandex.ru>
2017-11-16 20:14:58 +0400

    hxlt: add logging to express

--- hxlt/backend/express/src/server.js
+++ hxlt/backend/express/src/server.js
@@ -1,7 +1,10 @@
 import Express from 'express';
+import morgan from 'morgan';
 
 
 const app = new Express();
+const logger = morgan('combined');
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.send('ggwp lanaya\n');
