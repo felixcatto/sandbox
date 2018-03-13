ea07bfa879ebcc680a8760dc3582c07338972f2a
felixcatto <felix0808@yandex.ru>
2018-03-13 12:33:49 +0400

    redux: simplify config

--- react/redux/webpack.config.js
+++ react/redux/webpack.config.js
@@ -42,21 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-  );
-  config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true
-      },
       output: {
         comments: false,
       },
