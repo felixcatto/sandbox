3069eb327d6d15e8040ad75ec21c7f4ec43a9a2f
felixcatto <felix0808@yandex.ru>
2018-11-13 13:11:15 +0400

    oth: change increase font script

--- settings/chrome/increaseFont.js
+++ settings/chrome/increaseFont.js
@@ -1,6 +1,7 @@
 var p = [].slice.call(document.querySelectorAll('p'));
-var li = [].slice.call(document.querySelectorAll('li'));
-var els = p.concat(li);
+var code = [].slice.call(document.querySelectorAll('code'));
+var pre = [].slice.call(document.querySelectorAll('pre'));
+var els = p.concat(code).concat(pre);
 els.forEach(el => {
   el.style.fontSize = '20px';
   el.style.fontFamily = 'Roboto';
@@ -8,4 +9,6 @@ els.forEach(el => {
   el.style.marginBottom = '22px';
 });
 
-javascript:(function()%7Bvar p %3D %5B%5D.slice.call(document.querySelectorAll('p'))%3Bvar li %3D %5B%5D.slice.call(document.querySelectorAll('li'))%3Bvar els %3D p.concat(li)%3Bels.forEach(el %3D> %7Bel.style.fontSize %3D '20px'%3Bel.style.fontFamily %3D 'Roboto'%3Bel.style.lineHeight %3D 1.5%3Bel.style.marginBottom %3D '22px'%3B%7D)%7D)()
+https://mrcoles.com/bookmarklet/
+
+javascript:(function()%7Bvar%20p%20%3D%20%5B%5D.slice.call(document.querySelectorAll('p'))%3Bvar%20code%20%3D%20%5B%5D.slice.call(document.querySelectorAll('code'))%3Bvar%20pre%20%3D%20%5B%5D.slice.call(document.querySelectorAll('pre'))%3Bvar%20els%20%3D%20p.concat(code).concat(pre)%3Bels.forEach(el%20%3D%3E%20%7Bel.style.fontSize%20%3D%20'20px'%3Bel.style.fontFamily%20%3D%20'Roboto'%3Bel.style.lineHeight%20%3D%201.5%3Bel.style.marginBottom%20%3D%20'22px'%3B%7D)%7D)()
