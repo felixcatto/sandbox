1a4b7aa67e38b1ff5402c697ee52b699cd7b3a80
felixcatto <felix0808@yandex.ru>
2018-11-20 11:53:09 +0400

    gfrog: refactor

--- server/greenfrog/server/lib/formObjectBuilder.js
+++ server/greenfrog/server/lib/formObjectBuilder.js
@@ -1,10 +1,13 @@
-import { keyBy, pick } from 'lodash';
+import { pick } from 'lodash';
 
 
 export default (object, error = { errors: [] }, id = null) => ({
   id,
   ...object,
   errors: error.errors
-    .map(el => pick(el, ['path', 'message']))
-    |> ((_) => keyBy(_, 'path')),
+    .map(el => pick(el, ['message', 'path']))
+    |> (_ => _.reduce((acc, el) => ({
+      ...acc,
+      [el.path]: el.message,
+    }), {})),
 });
--- server/greenfrog/server/routes/session.js
+++ server/greenfrog/server/routes/session.js
@@ -1,4 +1,3 @@
-import buildFormObj from '../lib/formObjectBuilder';
 import { encrypt } from '../lib/secure';
 import { User } from '../models';
 import { emptyObject } from '../lib/utils';
@@ -7,8 +6,6 @@ import { emptyObject } from '../lib/utils';
 export default (router) => {
   router
     .get('newSession', '/session/new', async (ctx) => {
-      const data = {};
-      console.log(buildFormObj(data));
       ctx.render('sessions/new', {
         user: emptyObject,
       });
@@ -22,21 +19,19 @@ export default (router) => {
         },
       });
 
-      // user
-      //   ? console.log(user.get({ plain: true }))
-      //   : console.log(user);
-
       if (user?.passwordDigest === encrypt(password)) {
         ctx.session.userId = user.id;
         ctx.redirect(router.url('root'));
         return;
       }
 
-      // ctx.flash.set('email or password were wrong');
       ctx.render('sessions/new', {
         user: {
           email,
           password: '',
+          errors: {
+            email: 'email or password were wrong',
+          },
         },
       });
     })
