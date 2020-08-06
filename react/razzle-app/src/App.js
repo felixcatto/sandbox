60d97ea66efd8f8b0b4d2bc7eb93a8921d4d1ce3
felixcatto <felix0808@yandex.ru>
2020-08-06 11:22:28 +0400

    razzle: strip down

--- react/razzle-app/src/App.js
+++ react/razzle-app/src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import './App.css';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
-);
+const App = () => 322;
 
 export default App;
